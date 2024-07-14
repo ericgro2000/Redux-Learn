57d4059e8d83c457aea536316d1844af5f2b6886
ericgro2000 <eric.gro.2000@gmail.com>
2024-07-14 15:56:24 +0200

    Einfacher Zähler hinzugefügt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { FC } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { State } from "./main"
 
+const App: FC = () => {
+  const dispatch = useDispatch()
+  const cash = useSelector<State, number>(state => state.cash)
 
-function App() {
+  const addCash = (cash: number) => {
+    dispatch({ type: "ADD_CASH", payload: cash })
+  }
+
+  const getCash = (cash: number) => {
+    dispatch({ type: "GET_CASH", payload: cash })
+  }
 
   return (
-    <>
-    </>
+    <div className={'app'}>
+      <div style={{ fontSize: "2rem" }}>Amount:{cash}</div>
+      <div style={{ display: "flex" }}>
+        <button onClick={() => addCash(Number(prompt()))}>Deposit</button>
+        <button onClick={() => getCash(Number(prompt()))}>Withdraw</button>
+      </div>
+    </div>
   )
 }
 
