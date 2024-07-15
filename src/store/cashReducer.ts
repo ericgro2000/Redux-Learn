4ee46870a5de4a2f6ba6e6f66798d54f903545f6
ericgro2000 <eric.gro.2000@gmail.com>
2024-07-15 17:55:26 +0200

    commit for cleanin cash on stackblitz

---
+++ src/store/cashReducer.ts
@@ -0,0 +1,25 @@
+
+export interface State {
+    cash: number;
+  }
+  
+  interface action {
+    type: "ADD_CASH" | "GET_CASH";
+    payload: number;
+  }
+  
+  const defaultState: State = {
+    cash:0,
+  }
+  
+  export  const cashReducer = (state = defaultState, action:action) => {
+    switch (action.type) {
+    case "ADD_CASH":
+    return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+    return {...state, cash: state.cash - action.payload}
+    default:
+    return state
+    }
+  }
+  
---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { combineReducers, createStore } from "redux";
+import { cashReducer } from "./cashReducer";
+import { customerReducer } from "./customerReducer";
+
+const rootReducer = combineReducers({
+    cash:cashReducer,
+    customers:customerReducer
+})
+
+export const store = createStore(rootReducer)
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof createStore>
+export type AppDispatch = AppStore['dispatch']
