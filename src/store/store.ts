39781502876280a5d07df737d84b2499de791cea
ericgro2000 <eric.gro.2000@gmail.com>
2024-07-16 13:07:42 +0200

    test

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,8 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { cashReducer } from "./cashReducer";
 import { customerReducer } from "./customerReducer";
+import { composeWithDevTools } from "redux-devtools-extension"
+
 
 const rootReducer = combineReducers({
     cash:cashReducer,
